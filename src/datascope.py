194f7e0edb20cb04ef653a37d016e050def2a3f1
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-06-10 18:18:04 -0500

    modified hiring_confidence script to actually simulate financial differences when adding a n00b

--- src/datascope.py
+++ src/datascope.py
@@ -1,6 +1,7 @@
 import ConfigParser
 import random
 import os
+import collections
 
 import numpy
 
@@ -101,3 +102,55 @@ class Datascope(object):
         start somewhere.
         """
         return random.choice(self.historical_monthly_revenues)
+
+    def simulate_finances(self, n_months=12, n_universes=1000):
+        """Simulate finances for datascope to quantify a few significant
+        outcomes in what could happen.
+        """
+
+        # basically what we want to do is simulate starting with a certain amount in
+        # the bank, and getting paid X in any given month.
+        outcomes = collections.Counter()
+        end_cash = []
+        for universe in range(n_universes):
+            if universe % 100 == 0:
+                print "simulation %d" % universe
+            is_bankrupt = False
+            no_cash = False
+
+            # this game is a gross over simplification. each month datascope pays its
+            # expenses and gets paid at the end of the month. This is a terrifying way
+            # to run a business---we have quite a bit more information about the
+            # business health than "drawing a random number from a black box". For
+            # example, we have a sales pipeline, projects underway, and accounts
+            # receivable, all of which give us confidence about the current state of
+            # affairs beyond the cash on hand at the end of each month.
+            cash = initial_cash = self.n_months_buffer * self.costs()
+            for month in range(n_months):
+                revenue = self.simulate_revenue()
+                cash -= self.costs()
+                if cash < -self.line_of_credit:
+                    is_bankrupt = True
+                elif cash < 0:
+                    no_cash = True
+                cash += revenue
+            end_cash.append(cash)
+
+            # how'd we do this year? if we didn't go bankrupt, are we able to give a
+            # bonus? do we have excess profit beyond our target profit so we can grow
+            # the business
+            profit = cash - initial_cash
+            if is_bankrupt:
+                outcomes['bankrupt'] += 1
+            elif no_cash:
+                outcomes['survived with line of credit'] += 1
+            else:
+                outcomes['not bankrupt'] += 1
+                if profit < 0:
+                    outcomes['no bonus'] += 1
+                if profit > 0:
+                    outcomes['is bonus'] += 1
+                if profit > n_months * self.before_tax_profit():
+                    outcomes['can grow business'] += 1
+
+        return outcomes, end_cash
--- src/hiring_confidence.py
+++ src/hiring_confidence.py
@@ -5,64 +5,28 @@ TODO: how to reasonably decide when its a good time to hire, just based on our
 cash in the bank. probably something to do with ROI
 """
 
-import sys
-import collections
-from pprint import pprint
-
 from datascope import Datascope
 
 
-datascope = Datascope()
-
-# basically what we want to do is simulate starting with a certain amount in
-# the bank, and getting paid X in any given month.
 n_months = 12
-counter = collections.Counter()
-simulations = []
 n_universes = 1000
-for universe in range(n_universes):
-    if universe % 100 == 0:
-        print "simulation %d" % universe
-    is_bankrupt = False
-    no_cash = False
 
-    # this game is a gross over simplification. each month datascope pays its
-    # expenses and gets paid at the end of the month. This is a terrifying way
-    # to run a business---we have quite a bit more information about the
-    # business health than "drawing a random number from a black box". For
-    # example, we have a sales pipeline, projects underway, and accounts
-    # receivable, all of which give us confidence about the current state of
-    # affairs beyond the cash on hand at the end of each month.
-    cash = initial_cash = datascope.n_months_buffer * datascope.costs()
-    for month in range(n_months):
-        revenue = datascope.simulate_revenue()
-        cash -= datascope.costs()
-        if cash < -datascope.line_of_credit:
-            is_bankrupt = True
-        elif cash < 0:
-            no_cash = True
-        cash += revenue
-    simulations.append(cash)
+# simulate datascope revenues before adding a person
+datascope = Datascope()
+no_n00b_outcomes, no_n00b_cash = datascope.simulate_finances(
+    n_months=n_months,
+    n_universes=n_universes,
+)
 
-    # how'd we do this year? if we didn't go bankrupt, are we able to give a
-    # bonus? do we have excess profit beyond our target profit so we can grow
-    # the business
-    profit = cash - initial_cash
-    if is_bankrupt:
-        counter['bankrupt'] += 1
-    elif no_cash:
-        counter['survived with line of credit'] += 1
-    else:
-        counter['not bankrupt'] += 1
-        if profit < 0:
-            counter['no bonus'] += 1
-        if profit > 0:
-            counter['is bonus'] += 1
-        if profit > n_months * datascope.before_tax_profit():
-            counter['can grow business'] += 1
+# simulate revenues after adding a person
+datascope.add_person("joe")
+n00b_outcomes, n00b_cash = datascope.simulate_finances(
+    n_months=n_months,
+    n_universes=n_universes,
+)
 
-    # TODO: would be nice to look at some plots of these different scenarios
-
-#    print profit
-
-pprint(dict(counter), sys.stderr)
+keys = set(n00b_outcomes.keys()).union(set(no_n00b_outcomes.keys()))
+for key in keys:
+    no_n00b = no_n00b_outcomes[key]
+    n00b = n00b_outcomes[key]
+    print "%30s%10d%10d%10d" % (key, no_n00b, n00b, n00b - no_n00b)
