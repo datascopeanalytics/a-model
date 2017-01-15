57839b98fa28f1242fa1540cba4acafa1759827c
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2017-01-15 10:53:00 -0600

    using a markdown template to prepare monthly financials

---
+++ a_model/templates/monthly_financials.md
@@ -0,0 +1,4 @@
+## high level
+
+* YTD margin {{ "${:,.2f}".format(ytd_margin) }}
+  ({{ "{:.0f}%".format(100 * ytd_margin_growth) }})
--- bin/report_monthly_financials.py
+++ bin/report_monthly_financials.py
@@ -4,6 +4,12 @@ This script is used to quickly create financial summaries that can be imported
 into a slack post and shared with the broader team.
 """
 
+import jinja2
+env = jinja2.Environment(
+    loader=jinja2.PackageLoader('a_model', 'templates'),
+    autoescape=jinja2.select_autoescape(['html', 'xml'])
+)
+
 from a_model.company import Company
 from a_model.argparsers import ReportFinancialsParser
 
@@ -22,18 +28,5 @@ ytd_margin_growth = (ytd_margin - last_ytd_margin) / last_ytd_margin
 # last_ytd_revenue = company.profit_loss.get_ytd_revenue(months=-12)
 # ytd_revenue_growth = (ytd_revenue - last_ytd_revenue) / last_ytd_revenue
 
-output = """<html>
-<h2>high level</h2>
-<ul>
-<li> YTD margin $%(ytd_margin)d (%(ytd_margin_growth).1f)</li>
-</ul>
-</html>
-""" % locals()
-
-output = """
-## high level
-
-* YTD margin $%(ytd_margin)d (%(ytd_margin_growth).1f)
-""" % locals()
-
-print output
+template = env.get_template('monthly_financials.md')
+print template.render(**locals())
